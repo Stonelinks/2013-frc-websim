7a605df8359415888ec5dd0c6ee2305549299a21
Lucas Doyle <lucas.p.doyle@gmail.com>
2013-01-15 18:03:38 +0900

    removed server folder

--- server/frcsim/config.py
+++
@@ -1,9 +0,0 @@
-"""
-
-General settings
-
-"""
-
-app_name = '2013 First Robotics Simulator'
-
-local_server_port = 8089
--- server/server.py
+++
@@ -1,5 +0,0 @@
-import frcsim.server
-
-if __name__ == "__main__":
-  frcsim.server.main()
-
